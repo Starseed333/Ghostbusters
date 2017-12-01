6e62e8f5984c772474440ae7d8f9d346e76e5a48
Sandra Marquez <sandra@Sandras-MacBook-Air.local>
2017-12-01 17:56:01 -0600

    changed all code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# week-4-game
+# **Ghostbusters**
+
+### **Description**
+
+The application dynamically updates the HTML pages with the jQuery library in this Ghostbuster game.
+
+When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game. The player must then defeat all of the remaining fighters. Enemies are moved to a different area of the screen. The player chooses an opponent by clicking on an enemy's picture. Once the player selects an opponent, that enemy is moved to a defender area. The player will now be able to click the attack button. Whenever the player clicks attack, their character damages the defender. The opponent will lose HP (health points). These points are displayed at the bottom of the defender's picture. The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their HP. These points are shown at the bottom of the player character's picture.The player will keep hitting the attack button in an effort to defeat their opponent. The player wins the game by defeating all enemy characters. The player loses the game the game if their character's HP falls to zero or below. Go on and enjoy this fun game.
+
+
+**jQuery**
+
+jQuery is a fast, small, and feature-rich JavaScript library. It makes things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers. With a combination of versatility and extensibility, jQuery has changed the way that millions of people write JavaScript.
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,110 +1,300 @@
+// console.log('inside js');
 
-$(document).ready(function(){
-	
-	var targetNumber;
-	var random;
-	var ghostNumber;
-	var wins = 0;
-	var losses = 0;
+// ----- Global Variables ----- //
+
+// Has the user selected their character
+var characterSelected = false;
+
+// Has the user selected the defender
+var defenderSelected = false;
+
+// Variable to store the user's chosen character
+var character = {};
+
+// Variable to store the chosen enemy
+var defender = {};
+
+// Number of enemies defeated
+var enemiesDefeated = 0;
+
+// Boolean to indicate whether or not the game is over
+gameOver = false;
 
 
-	function gameUpdate(val){
-			if(targetNumber === ghostNumber){
-				wins++
-				alert("youw win");
-				$("#wins").html("<h4>" + wins + "</h4>");
-				console.log(wins);
-		}
-			else{(targetNumber !== ghostNumber)
-				losses++
-				$("#losses").html("<h4>" + losses + "</h4>");
-		}
-			if(targetNumber === vigoNumber){
-				wins++
-				$("#wins").html("<h4>" + wins + "</h4>");
-		}
-			else{(targetNumber !== vigoNumber)
-				losses++
-				$("#losses").html("<h4>" + losses + "</h4>");
-		}
-			if(targetNumber === samNumber){
-				wins++
-				$("#wins").html("<h4>" + wins + "</h4>");
-		}
-			else{(targetNumber !== samNumber)
-				losses++
-				$("#losses").html("<h4>" + losses + "</h4>");
-		}
-			if(targetNumber === greenNumber){
-				wins++
-				$("#wins").html("<h4>" + wins + "</h4>");
-		}
-			else{(targetNumber !== greenNumber)
-				losses++
-				$("#losses").html("<h4>" + losses + "</h4>");
-		}
+
+var ghostbuster = {
+  name: "ghostbuster",
+  health: 120,
+  baseAttack: 8,
+  attack: 8
+};
+
+var vigo = {
+  name: "vigo",
+  health: 100,
+  baseAttack: 5,
+  attack: 5
+};
+
+var samhain = {
+  name: "samhain",
+  health: 150,
+  baseAttack: 20,
+  attack: 20
+};
+
+var greenslime = {
+  name: "greenslime",
+  health: 180,
+  baseAttack: 25,
+  attack: 25
+};
+
+
+
+// This function will initialize the character value from the global object variables defined above
+function initializeCharacter(chosenCharacter) {
+  character.name = chosenCharacter.name;
+  character.health = chosenCharacter.health;
+  character.baseAttack = chosenCharacter.baseAttack;
+  character.attack = chosenCharacter.attack;
+}
+
+// This function will initialize the enemy's value from the global object variables defined above
+function initializeDefender(chosenDefender) {
+  defender.name = chosenDefender.name;
+  defender.health = chosenDefender.health;
+  defender.baseAttack = chosenDefender.baseAttack;
+  defender.attack = chosenDefender.attack;
+}
+
+// This function will move the remaining characters to the enemies section
+function moveToEnemies() {
+  $(".available-character").removeClass("available-character").addClass("enemy-character");
+  $("#enemies-available").append($(".enemy-character"));
+}
+
+// This function will reset the state of the game
+function resetGame() {
+  // Reset all the health values to the original
+  $("#ghostbuster-character").children(".health").html(ghostbuster.health);
+  $("#vigo-character").children(".health").html(vigo.health);
+  $("#samhain-character").children(".health").html(samhain.health);
+  $("#greenslime-character").children(".health").html(greenslime.health);
+
+  $(".character-image").removeClass("chosen-character enemy-character defender-character").addClass("available-character");
+  var available = $(".available-character").show();
+  $("#characters-available").html(available);
+
+  $("#game-message").empty();
+  $("#restart").hide();
+
+  characterSelected = false;
+  defenderSelected = false;
+  enemiesDefeated = 0;
+  gameOver = false;
+
+  character = {};
+  defender = {};
 }
 
 
 
 
-	$("#target-score").on("click", function (){
-			targetNumber = "";
-			for (var i = 0; i <1; i++){
-				random = Math.floor(Math.random() *20)+1;
-				targetNumber = random + targetNumber;
-				$("#target-score").html("Power to be collected " + targetNumber);
-			}
+$(document).ready(function() {
+  $("#restart").hide();
 
-		});
+  // Determine which character the user has clicked
+  $("#ghostbuster-character").on("click", function () {
+    console.log("Ghosbusters are selected");
 
+    // User is choosing the character
+    if(characterSelected == false) {
+      $("#game-message").empty();
 
+      // Set the user's character
+      initializeCharacter(ghostbuster);
+      characterSelected = true;
 
-		$("#ghostbuster").on("click", function (){
-			ghostNumber = "";
-			for (var i = 0; i < 1; i++){
-				var randomNumber = Math.floor(Math.random() *10) +1;
-				ghostNumber = randomNumber + ghostNumber;
-				$("#ghostbusterscore").html("Total power " + ghostNumber);
-			}
-		});
-	
+      // Display the chosen character
+      $("#ghostbuster-character").removeClass("available-character").addClass("chosen-character");
+      $("#chosen-character").append(this);
 
-		$("#vigo").on("click", function (){
-			var vigoNumber = "";
-			for (var i = 0; i < 1; i++){
-				var randomNumber = Math.floor(Math.random() *10) +1;
-				vigoNumber = randomNumber + vigoNumber;
-				$("#vigoscore").html("Total power " + vigoNumber);
-			}
-		});
-	
+      // Move the remaining characters to the enemies section
+      moveToEnemies();
+    } else if ((characterSelected == true) && (defenderSelected == false)) {
+      // User is choosing the defender
+      if($("#ghostbuster-character").hasClass("enemy-character")) {
+        $("#game-message").empty();
 
-		$("#samhain").on("click", function (){
-			var samNumber = "";
-			for (var i = 0; i < 1; i++){
-				var randomNumber = Math.floor(Math.random() *10) +1;
-				samNumber = randomNumber + samNumber;
-				$("#samhainscore").html("Total power " + samNumber);
-			}
-		});
+        // Set the user's enemy
+        initializeDefender(ghostbuster);
+        defenderSelected = true;
 
-		$("#greenslime").on("click", function (){
-			var greenNumber = "";
-			for (var i = 0; i < 1; i++){
-				var randomNumber = Math.floor(Math.random() *10) +1;
-				greenNumber = randomNumber + greenNumber;
-				$("#greenslimescore").html("Total power " + greenNumber);
-			}
-		});
+        // Add the character to the defender section
+        $("#ghostbuster-character").removeClass("enemy-character").addClass("defender-character");
+        $("#defender-section").append(this);
+      }
+    }
+  });
+  // characters
+  $("#vigo-character").on("click", function () {
+    console.log("vigo is selected");
 
-});
+    // User is choosing the character
+    if(characterSelected == false) {
+      $("#game-message").empty();
 
+      // Set the user's character
+      initializeCharacter(vigo);
+      characterSelected = true;
 
+      // Display the chosen character
+      $("#vigo-character").removeClass("available-character").addClass("chosen-character");
+      $("#chosen-character").append(this);
 
+      // Move the remaining characters to the enemies section
+      moveToEnemies();
+    } else if ((characterSelected == true) && (defenderSelected == false)) {
+      // User is choosing the defender
+      if($("#vigo-character").hasClass("enemy-character")) {
+        $("#game-message").empty();
 
+        // Set the user's enemy
+        initializeDefender(vigo);
+        defenderSelected = true;
 
+        // Add the character to the defender section 
+        $("#vigo-character").removeClass("enemy-character").addClass("defender-character");
+        $("#defender-section").append(this);
+      }
+    }
+  });
 
+  $("#samhain-character").on("click", function () {
+    console.log("Samhahin is selected");
 
+    // User is choosing the character
+    if(characterSelected == false) {
+      $("#game-message").empty();
 
+      // Set the user's character
+      initializeCharacter(samhain);
+      characterSelected = true;
+
+      // Display the chosen character
+      $("#samhain-character").removeClass("available-character").addClass("chosen-character");
+      $("#chosen-character").append(this);
+
+      // Move the remaining characters to the enemies section
+      moveToEnemies();
+    } else if ((characterSelected == true) && (defenderSelected == false)) {
+      // User is choosing the defender
+      if($("#samhain-character").hasClass("enemy-character")) {
+        $("#game-message").empty();
+
+        // Set the user's enemy
+        initializeDefender(samhain);
+        defenderSelected = true;
+
+        // Add the character to the defender section 
+        $("#darth-sidious-character").removeClass("enemy-character").addClass("defender-character");
+        $("#defender-section").append(this);
+      }
+    }
+  });
+
+  $("#greenslime-character").on("click", function () {
+    console.log("Greenslime is selected");
+
+    // User is choosing the character
+    if(characterSelected == false) {
+      $("#game-message").empty();
+
+      // Set the user's character
+      initializeCharacter(greenslime);
+      characterSelected = true;
+
+      // Display the chosen character
+      $("#greenslime-character").removeClass("available-character").addClass("chosen-character");
+      $("#chosen-character").append(this);
+
+      // Move the remaining characters to the enemies section
+      moveToEnemies();
+    } else if ((characterSelected == true) && (defenderSelected == false)) {
+      // User is choosing the defender
+      if($("#greenslime-character").hasClass("enemy-character")) {
+        $("#game-message").empty();
+
+        // Set the user's enemy
+        initializeDefender(greenslime);
+        defenderSelected = true;
+
+        // Add the character to the defender section 
+        $("#greenslime-character").removeClass("enemy-character").addClass("defender-character");
+        $("#defender-section").append(this);
+      }
+    }
+  });
+
+  $("#attack").on("click", function() {
+    console.log("Attack selected");
+
+    console.log("character = " + JSON.stringify(character));
+    console.log("defender = " + JSON.stringify(defender));
+
+    // User is ready to attack the defender
+    if (characterSelected && defenderSelected && !gameOver) {
+      // User attacks the defender and decreases the defender's health points
+      defender.health = defender.health - character.attack;
+      $(".defender-character").children(".health").html(defender.health);
+      $("#game-message").html("<p>You attacked " + defender.name + " for " + character.attack + " damage.<p>");
+
+      // User's attack power increases
+      character.attack = character.attack + character.baseAttack;
+
+      // If defender is still alive, they counter attack the user
+      if (defender.health > 0) {
+        character.health = character.health - defender.baseAttack;
+        $(".chosen-character").children(".health").html(character.health);
+
+        // Check if the user survives the attack
+        if (character.health > 0) {
+          $("#game-message").append("<p>" + defender.name + " attacked you back for " + defender.baseAttack + " damage.</p>");
+        } else {
+          gameOver = true;
+          $("#game-message").html("<p>You were defeated... womp womp...</p><p>Play again?</p>");
+          $("#restart").show();
+        }
+      } else {
+        // Defender is defeated
+        enemiesDefeated++;
+        defenderSelected = false;
+        $("#game-message").html("<p>You have defeated " + defender.name + ". Choose another enemy.</p>");
+        $(".defender-character").hide();
+
+        // Check if the user has won the game
+        if (enemiesDefeated === 3) {
+          gameOver = true;
+          $("#game-message").html("<p>You have won the game!!!</p><p>Play again?</p>");
+          $("#restart").show();
+        }
+      }
+    } else if (!characterSelected && !gameOver) {
+      $("#game-message").html("<p>You must first select your game character.</p>");
+    } else if (!defenderSelected && !gameOver) {
+      $("#game-message").html("<p>You must choose an enemy to fight.</p>");
+    }
+
+    console.log("character = " + JSON.stringify(character));
+    console.log("defender = " + JSON.stringify(defender));
+  });
+
+  $("#restart").on("click", function() {
+    console.log("Restart selected");
+
+    resetGame();
+  });
+
+}); 
 
